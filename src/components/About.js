1292bb9fcdc00f7ba3e158518367f7205053b324
Trisha Wang <trish.regine@gmail.com>
2023-03-15 00:36:49 +0800

    Added data js for About

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "../data/data_about";
 
 const About = () => {
+  const [about, setAbout] = useState(data);
   return (
     <div id='about'>
       <div className='max-w-[70vw] my-[10vw]'>
@@ -8,20 +10,10 @@ const About = () => {
           About me
           <span>.</span>
         </h1>
-        <p>
-          Hi, Im Trisha, an artist at heart. Passionate about innovation,
-          creating new things, and using my skills to improve quality of
-          people’s lives (like a fairy godmother of sorts? heh).
-        </p>
-        <p>
-          I have a Bachelors Degree in Computer Science from University of Santo
-          Thomas.
-        </p>
-        <p>
-          I'm happiest when I'm creating, learning, exploring and thinking about
-          how to make things better. I'm not available for any freelance work,
-          but feel free to reach out and say hello! I promise I don't bite 😉
-        </p>
+        {about.map((about) => {
+          const { id, paragraph } = about;
+          return <p kay={id}>{paragraph}</p>;
+        })}
       </div>
     </div>
   );
---
+++ src/data/data_about.js
@@ -0,0 +1,19 @@
+const data_about = [
+  {
+    id: 1,
+    paragraph:
+      "Hi, Im Trisha, an artist at heart. Passionate about innovation, creating new things, and using my skills to improve quality of people’s lives (like a fairy godmother of sorts? heh).",
+  },
+  {
+    id: 2,
+    paragraph:
+      "I have a Bachelors Degree in Computer Science from University of Santo Thomas. And still continuously expanding my knowledge and skills now that we're the digital age. Every information is at everyone’s fingertips ☝️",
+  },
+  {
+    id: 3,
+    paragraph:
+      "I'm happiest when I'm creating, learning, exploring and thinking about how to make things better. I'm not available for any freelance work, but feel free to reach out and say hello! I promise I don't bite 😉",
+  },
+];
+
+export default data_about;
